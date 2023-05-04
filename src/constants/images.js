e64335cd0a5ed4802f22b4bb6f84301c633b7f04
Prashant Kumar Singhcd <pk3076889@gmail.com>
2023-05-05 03:51:16 +0530

    images error fixed

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,16 +1,16 @@
 import message from './../assets/message.svg';
 
-import company1 from './../assets/company/company1.svg';
-import company2 from './../assets/company/company2.svg';
-import company3 from './../assets/company/company3.svg';
-import company4 from './../assets/company/company4.svg';
-import company5 from './../assets/company/company5.svg';
-import company6 from './../assets/company/company6.svg';
-import company7 from './../assets/company/company7.svg';
-import company8 from './../assets/company/company8.svg';
-import company9 from './../assets/company/company9.svg';
-import company10 from './../assets/company/company10.svg';
-import company11 from './../assets/company/company11.svg';
+import company1 from './../assets/company/Company1.svg';
+import company2 from './../assets/company/Company2.svg';
+import company3 from './../assets/company/Company3.svg';
+import company4 from './../assets/company/Company4.svg';
+import company5 from './../assets/company/Company5.svg';
+import company6 from './../assets/company/Company6.svg';
+import company7 from './../assets/company/Company7.svg';
+import company8 from './../assets/company/Company8.svg';
+import company9 from './../assets/company/Company9.svg';
+import company10 from './../assets/company/Company10.svg';
+import company11 from './../assets/company/Company11.svg';
 
 import work1 from './../assets/work/work1.svg';
 import work2 from './../assets/work/work2.svg';
